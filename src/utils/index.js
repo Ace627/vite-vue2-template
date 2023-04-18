8d093883f9eac45a2e6318c41ece3f284a6d7595
Legendary <1207588603@qq.com>
2023-04-18 22:49:48 +0800

    全局方法注释更新

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,62 +1,84 @@
-// 获取随机 Hex 颜色
+/**
+ * 获取随机 Hex 颜色
+ * @returns {string} #f1f1f1
+ */
 export function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 0xffffff).toString(16)}`
 }
 
-// 获取随机 RGB 颜色
+/**
+ * 获取随机 RGB 颜色
+ * @returns {string} rgb(150, 150, 150)
+ */
 export function getRandomRgbColor() {
   const random = () => Math.floor(Math.random() * 256)
   return `rgb(${random()}, ${random()}, ${random()})`
 }
 
-// RGB 颜色转 Hex 格式
+/**
+ * RGB 颜色转 Hex 格式
+ * @param {number} r 红色
+ * @param {number} g 绿色
+ * @param {number} b 蓝色
+ * @returns {string} #f1f1f1
+ */
 export function RGBToHex(r, g, b) {
   return '#' + ((r << 16) + (g << 8) + b).toString(16).padStart(6, '0')
 }
 
-// 获取变量的原始类型
+/**
+ * 获取变量的原始类型
+ * @param {any} variable 任意数据类型
+ * @returns {string} string | number | array | date ...
+ */
 export function getVariableRawType(variable) {
   return Object.prototype.toString.call(variable).split(' ')[1].replace(']', '').toLowerCase()
 }
 
-// 获取随机布尔值
+/**
+ * 获取随机布尔值
+ * @returns {boolean} true | false
+ */
 export function getRandomBoolean() {
   return Math.random() >= 0.5
 }
 
-// 反转字符串
+/**
+ * 反转字符串
+ * @param {string} str 准备进行反转的字符串
+ * @returns {string} 反转后的字符串
+ */
 export function reverseString(str) {
   return str.split('').reverse().join('')
 }
 
-// 字符串重复指定次数
+/**
+ * 字符串重复指定次数
+ * @param {string} str 准备进行重复的字符串
+ * @returns {string} 重复后的字符串
+ */
 export function repeatString(str, count) {
   return new Array(count).fill(str).join('')
 }
 
-// 字符串首字母大写
+/**
+ * 字符串首字母大写
+ * @param {string} str 准备进行首字母大写的字符串
+ * @returns {string} 首字母大写后的字符串
+ */
 export function upperStringFirst(str) {
   return str.charAt(0).toUpperCase() + str.substring(1)
 }
 
-// 深拷贝
+/**
+ * 深拷贝，支持日期、正则、函数
+ * @param {[key:string]: any} source 拷贝数据源
+ * @returns {[key:string]: any} 拷贝后的数据源
+ */
 export function deepClone(source) {
-  // 当待拷贝数据为 null NaN undefined number string 等基本数据类型时直接返回
-  if (!source || typeof source !== 'object') {
-    return source
-  }
-  // Date 类型
-  if (source instanceof Date) {
-    const tmpDate = new Date()
-    tmpDate.setTime(new Date().getTime())
-    return tmpDate
-  }
-  // 正则类型类型
-  if (source instanceof RegExp) {
-    const Constructor = source.constructor
-    return new Constructor(source)
-  }
-  // 如果是数组等引用数据类型
+  if (!source || typeof source !== 'object') return source
+  if (source instanceof Date) return new Date(source)
+  if (source instanceof RegExp) return new RegExp(source)
   const target = Array.isArray(source) ? [] : {}
   for (const key in source) {
     target[key] = typeof source[key] === 'object' ? deepClone(source[key]) : source[key]
